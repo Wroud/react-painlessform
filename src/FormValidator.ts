aa6eddae4407f6bd8978d5fedcd2e4e4e75c8bad
Wroud <wrouds@gmail.com>
2018-04-18 22:18:08 +0300

    add Yup support to createFormValidator

--- src/FormValidator.ts
+++ src/FormValidator.ts
@@ -1,3 +1,4 @@
+import * as Yup from "yup";
 import { IValidator, Validator } from "./ArrayValidator";
 import { mergeFormErrors } from "./tools";
 
@@ -6,23 +7,40 @@ export type FormErrors<T> = {
 };
 
 export class FormValidator<TSource, TMeta = {}> implements IValidator<TSource, FormErrors<TSource>, TMeta> {
-    private validators: Array<IValidator<TSource, FormErrors<TSource>, TMeta>>;
+    private validators: Array<IValidator<TSource, FormErrors<TSource>, TMeta> | Yup.Schema<any>>;
 
-    constructor(...validators: Array<IValidator<TSource, FormErrors<TSource>, TMeta>>) {
+    constructor(...validators: Array<IValidator<TSource, FormErrors<TSource>, TMeta> | Yup.Schema<any>>) {
         this.validators = validators;
     }
 
     validate = (data: TSource, meta: TMeta) => {
         let errors = {};
         this.validators.forEach(validator => {
-            errors = mergeFormErrors(errors, validator.validate(data, meta));
+            if ((validator as Yup.Schema<any>).validateSync) {
+                try {
+                    (validator as Yup.Schema<any>).validateSync(data, {
+                        abortEarly: false,
+                        context: meta || {},
+                    });
+                } catch (_errors) { // : Yup.ValidationError
+                    if (_errors.path === undefined) {
+                        _errors.inner.forEach(error => {
+                            errors = mergeFormErrors(errors, { [error.path]: error.errors });
+                        });
+                    }//  else {
+                    // this.context = _errors.errors;
+                    // }
+                }
+            } else {
+                errors = mergeFormErrors(errors, (validator as IValidator<TSource, FormErrors<TSource>, TMeta>).validate(data, meta));
+            }
         });
         return errors as FormErrors<TSource>;
     }
 }
 
 export function createFormValidator<TSource, TMeta = {}>(
-    ...validators: Array<IValidator<TSource, FormErrors<TSource>, TMeta>>,
+    ...validators: Array<IValidator<TSource, FormErrors<TSource>, TMeta> | Yup.Schema<any>>,
 ): IValidator<TSource, FormErrors<TSource>, TMeta> {
     return new FormValidator(...validators);
 }
