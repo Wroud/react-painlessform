e2df6ed15c9cf288485035c259fcc99b527049de
Wroud <wrouds@gmail.com>
2018-06-11 00:33:09 +0300

    add ts example

---
+++ examples/ts/src/index.tsx
@@ -0,0 +1,19 @@
+import * as React from "react";
+import { render } from "react-dom";
+import Hello from "./Hello";
+import registerServiceWorker from "./registerServiceWorker";
+
+const styles = {
+  fontFamily: "sans-serif",
+  textAlign: "center"
+};
+
+const App = () => (
+  <div style={styles}>
+    <Hello name="CodeSandbox" />
+    <h2>Start editing to see some magic happen {"\u2728"}</h2>
+  </div>
+);
+
+render(<App />, document.getElementById("root"));
+registerServiceWorker();
