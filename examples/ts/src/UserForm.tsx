74c0a20bc0059a0f2fa72042cb73c69625d3d76f
Wroud <wrouds@gmail.com>
2018-06-13 21:43:35 +0300

    update example

--- examples/ts/src/UserForm.tsx
+++ examples/ts/src/UserForm.tsx
@@ -1,8 +1,7 @@
 import * as React from "react";
 import { isString } from "util";
 import * as Yup from "yup";
-import { createFormFactory, FieldValue, IFormStorage, IsField, IUpdateEvent } from "react-painlessform"; // "react-painlessform";
-import { Path } from "react-painlessform";
+import { Path, createFormFactory, FieldValue, IFormStorage, IsField, IUpdateEvent } from "react-painlessform"; // "react-painlessform";
 import { TextField } from "./FormPrimitives";
 
 export interface IUser {
